8499583b6c02dc36047506bb7cfca00abf17d747
mohdadil2k <waheedhaasif2000@gmail.com>
2022-09-23 17:47:50 +0530

    added user location search feature

--- app.js
+++ app.js
@@ -1,7 +1,4 @@
-let ac=document.getElementById('input');
-let span=document.getElementById('span');
-
-
+const location_element=document.querySelector('.location');
 const inputField=document.getElementById("inputField");
 const searchButton=document.querySelector(".button")
 const city_h1=document.getElementById("city_h1");
@@ -14,6 +11,23 @@ const windspeed_p=document.getElementById("windSpeed_p");
 //https://source.unsplash.com/1600x900/?Chennai
 const weather={
     "API":"a5810fc9d2b5de67b5415fac0cf6c07e",
+    fetchLat:function(lat,long){
+        fetch(`https://api.geoapify.com/v1/geocode/reverse?lat=${lat}&lon=${long}&apiKey=a26b661efecb442796247bd8a7de99f1`)
+        .then(response => response.json())
+        .then(result => weather.fetchWeather(result.features[0].properties.city))
+        .catch(error => console.log('error', error));
+    },
+    knowLocation:function(){
+        let fetchLat=weather.fetchLat;
+        return navigator.geolocation.getCurrentPosition((success)=>{
+            let coords=success.coords;
+            lat=coords.latitude;
+            long=coords.longitude;
+            fetchLat(lat,long)
+        },(error)=>{
+            alert(error.message);
+        });
+    },
     fetchWeather:function(city){
             fetch("https://api.openweathermap.org/data/2.5/weather?q="
             +city
@@ -61,5 +75,5 @@ inputField.addEventListener('keyup',(event)=>{
         alert('Please Enter Your City or State or Country')
     }
 })
-
-weather.fetchWeather("chennai")
+location_element.addEventListener('click',weather.knowLocation)
+weather.fetchWeather("trichy")
